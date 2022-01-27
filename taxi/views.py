80d6e410c90ee7cacaa203d816d75002139cb04d
PSzczepanski1996 <p.szczepanski996@gmail.com>
2022-01-27 22:19:59 +0100

    Add sorting by logged in

--- taxi/views.py
+++ taxi/views.py
@@ -3,6 +3,7 @@
 # Create your views here.
 from datetime import timedelta
 
+from django.db.models import Case, When
 from django.http import JsonResponse
 from django.template.loader import render_to_string
 from django.utils import timezone
@@ -41,15 +42,23 @@ class RobotsView(TemplateView):  # noqa: D101
 @csrf_exempt
 def load_drivers(request):
     get_drivers = []
-    for key, value in state_dict.items():
-        if value > timezone.now() - timedelta(minutes=5):
-            get_drivers.append(key)
     if request.POST['filter'] == 'all':
         drivers = Kierowca.objects.all()
     else:
+        for key, value in state_dict.items():
+            if value > timezone.now() - timedelta(minutes=5):
+                get_drivers.append(key)
         drivers = Kierowca.objects.filter(
-            id__in=get_drivers,
+            idKierowcy__in=get_drivers,
         ).exclude(usluga__in=Usluga.objects.filter(statusRealizacji__in=[Usluga.ROZPOCZETO, Usluga.W_TRAKCIE]))
+    ids = list(drivers.values_list('idKierowcy', flat=True))
+    ordered_ids = []
+    for id in ids:
+        if id in state_dict.keys():
+            ordered_ids.append(id)
+    [ordered_ids.append(id) for id in ids if id not in ordered_ids]
+    preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(ordered_ids)])
+    drivers = Kierowca.objects.filter(idKierowcy__in=ordered_ids).order_by(preserved)
     template = 'drivers.html'
     body = render_to_string(
         template,
